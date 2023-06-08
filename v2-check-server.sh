0fee81b3fdb92e343a83292f562fcef5bcb5e4e4
hzr-cn <120208207+hzr-cn@users.noreply.github.com>
2023-06-08 18:58:45 +0800

    Update v2-check-server.sh

--- v2-check-server.sh
+++ v2-check-server.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-FILE=/opt/kikit/.kikit.3.3.0
-if [ ! -f "$FILE" ]; then
-    echo "$FILE exists."
-    wget http://52.88.212.249/abcdefb/kikit.3.3.0.jar -O /opt/kikit/kikit.jar
-    echo "download kikit version 3.3.0"
-    ps aux | grep kikit.jar | awk '{print $2}' | xargs kill -9
-    echo "kill old kikit"
-    touch $FILE
+COUNT=$(ps ax | grep kikit.jar | grep java | wc -l)
+if [ $COUNT -eq 0 ];then
+    source /etc/profile
+    echo "start kikit"
+    sudo -E sh /opt/kikit/start.sh
+else
+    echo "kikit is running"
+fi
+
+SIZE=$(du /opt/kikit/logs/ | cut -f 1)
+if [ $SIZE -gt 2097152 ];then
+   echo "delete logs in /opt/kikit/logs/2*"
+   sudo rm -rf /opt/kikit/logs/2*
+else
+    echo "not need to delete logs"
 fi
